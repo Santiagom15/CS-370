80c4db6bdba9b3cc8dd00208169f8c6ce18e6e33
cs329quiz0 <astarr4@emory.edu>
2023-10-03 22:29:48 -0400

    Added animated objects including blood drips/splatters

---
+++ cs 370/Level 1/Animated haunted mirror 2.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+# Animate the haunted mirror in the bathroom in level 1 setting
+
+# Get the node with the fox animations for the animation sprite
+@onready var anim = get_node("AnimatedSprite2D")
+
+# Play the animation when the scene loads 
+func _ready():
+	anim.play("Haunted mirror 2")
