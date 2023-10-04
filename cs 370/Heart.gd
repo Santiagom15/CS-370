7bc4febee3eeef108cc89737cfdf3187678589d9
Labdhi Mehta <lmehta@emory.edu>
2023-10-04 02:12:24 -0400

    boss fight heart demo

--- cs 370/Heart.gd
+++ cs 370/Heart.gd
@@ -1,6 +1,35 @@
-extends 
-@onready var _animation_player = $AnimationPlayer
+extends CharacterBody2D
 
+
+@onready var _animated_sprite = $AnimatedSprite2D
+
+@onready var timmy = $AudioStreamPlayer2D
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+	# Add the gravity.
+	#if not is_on_floor():
+		#velocity.y += gravity * delta
+
+	# Handle Jump.
+	#if Input.is_action_just_pressed("ui_accept"): #and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 func _process(_delta):
-	_animation_player.play("heart")
-
+	$AudioStreamPlayer2D.get_time()
+	var t = int(timmy.time)
+	print("Time is: ", t)
+	if(t < 15):
+		if(t % 4 == 0):
+			_animated_sprite.play("beat")
+	elif(t < 20):
+		_animated_sprite.play("beat")
+	else:
+		if(t % 2 == 0):
+			_animated_sprite.play("beat")
+		
+	
