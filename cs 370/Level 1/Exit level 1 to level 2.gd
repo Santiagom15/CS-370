07f8b6a66de3f6c95f9b59971155ae6465d8fc08
cs329quiz0 <astarr4@emory.edu>
2023-10-04 18:35:40 -0400

    Force push

--- cs 370/Level 1/Exit level 1 to level 2.gd
+++ cs 370/Level 1/Exit level 1 to level 2.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 # Detect when the player has reached the end of the stairs in Level 1
 # and automatically change scenes to Level 2
 
-# Detect when the player comes into/collides with the "Player detection" collision shape
+# Detect when the player comes into/collides with the "Player detection"
+# collision shape
 func _on_player_detection_body_entered(body):
-	# Check if the body/node that has collided with Player decection shape is named "Player"
+	# Check if the body/node that has collided with Player decection
+	#  shape is named "Player"
 	if body.name == "Player":
 		# Change the scene to the next floor
 		get_tree().change_scene_to_file("res://Level 2/floor4.tscn")
