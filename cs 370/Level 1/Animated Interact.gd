fb02a5b1f5eff2bb6f62c20bf0f1d3d21af5f42a
cs329quiz0 <astarr4@emory.edu>
2023-10-10 00:35:30 -0400

    Implementation of interactable door from both directions, some logical bugs

--- cs 370/Level 1/Animated Interact.gd
+++ cs 370/Level 1/Animated Interact.gd
@@ -1,32 +1,60 @@
 extends CharacterBody2D
 
-var collDoor : CollisionShape2D
-var collDetect : CollisionShape2D
-var in_coll = false
-@onready var anim = $AnimatedSprite2D
+@onready var collDoor = $CollDoor
+@onready var animRight = $AnimatedDoorRight
+@onready var animLeft = $AnimatedDoorLeft
 
-func _ready():
-	collDoor = $CollDoor  # Reference to the CollisionShape2D node
-	collDetect = $PlayerDetection/CollisionShape2D
-	
+var in_coll = false
+var coll_from_left = true  # true when door approached on the left, false when approached from the right
+
+
 func _process(delta):
 	if in_coll == true && Input.is_action_pressed("ui_accept"):
-		anim.play("Interact")
-		collDoor.set_deferred("disabled", true)  # Disable the CollisionShape2D
-		collDetect.set_deferred("disabled", true)
-	if $CollDoor.disabled == false:
-		print("entered collDoor is enabled")
-	else:
-		print("entered collDoor is disabled")
-	if $PlayerDetection/CollisionShape2D.disabled == false:
-		print("entered collDetect is enabled")
-	else:
-		print("entered collDetect is disabled")
+		if coll_from_left == true:
+			animRight.play("OpenRight")
+			collDoor.set_deferred("disabled", true)  # Disable the CollisionShape2D
+			in_coll = false
+		else:
+			animLeft.play("OpenLeft")
+			collDoor.set_deferred("disabled", true)  # Disable the CollisionShape2D
+			in_coll = false
+		#collDetect.set_deferred("disabled", true)
+#	elif in_coll == false:
+#		collDoor.set_deferred("disabled", false)  # Disable the CollisionShape2D
+#		collDetect.set_deferred("disabled", false)
+		
+#	if $CollDoor.disabled == false:
+#		print("entered collDoor is enabled")
+#	else:
+#		print("entered collDoor is disabled")
+#	if $PlayerDetection/CollisionShape2D.disabled == false:
+#		print("entered collDetect is enabled")
+#	else:
+#		print("entered collDetect is disabled")
 	
 
-func _on_player_detection_body_entered(body):
+func _on_PlayerDetectionFromLeft_body_entered(body):
+	print("Entered")
 	if body.name == "Player":  # Check if the colliding body is the player
 		in_coll = true
+		
+func _on_PlayerDetectionFromRight_body_entered(body):
+	pass # Replace with function body.
 
-func _on_player_detection_body_exited(body):
-	pass
+func _on_PlayerDetectionFromLeft_body_exited(body):
+	print("Exited")
+	if body.name == "Player" && in_coll == false:
+		if coll_from_left == true:
+			animRight.play("CloseRight")
+			collDoor.set_deferred("disabled", false)  # Disable the CollisionShape2D
+			in_coll = false
+			coll_from_left = false
+		else:
+			animLeft.play("CloseLeft")
+			collDoor.set_deferred("disabled", false)  # Disable the CollisionShape2D
+			in_coll = false
+			coll_from_left = true
+
+
+func _on_PlayerDetectionFromRight_body_exited(body):
+	pass # Replace with function body.
