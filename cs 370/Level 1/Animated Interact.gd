71230ad7db360ee32f4d33a9e7f8a452d5153d30
cs329quiz0 <astarr4@emory.edu>
2023-10-10 02:57:48 -0400

    Working two sided interactable door. Very complicated method and debugging print statements included

--- cs 370/Level 1/Animated Interact.gd
+++ cs 370/Level 1/Animated Interact.gd
@@ -1,60 +1,117 @@
 extends CharacterBody2D
 
 @onready var collDoor = $CollDoor
+@onready var detectLeft = $PlayerDetectionFromLeft/CollisionShape2D
+@onready var detectRight = $PlayerDetectionFromRight/CollisionShape2D
 @onready var animRight = $AnimatedDoorRight
 @onready var animLeft = $AnimatedDoorLeft
 
-var in_coll = false
-var coll_from_left = true  # true when door approached on the left, false when approached from the right
+var play_open = false
+var play_close = false
+var left_open = false
+var right_open = false
 
+# Stack to record current position of player relative to door
+# Value of 0 means player is to the left for the door, 1 means to the right
+var stack_approach_side : Array = []
+var len : int
+var curr_side : int
+ 
 
 func _process(delta):
-	if in_coll == true && Input.is_action_pressed("ui_accept"):
-		if coll_from_left == true:
+	len = stack_approach_side.size()
+	if play_open == true && len == 2 && Input.is_action_pressed("ui_accept"):
+		curr_side = stack_approach_side[len - 1]
+		if curr_side == 0 && right_open == false:
 			animRight.play("OpenRight")
 			collDoor.set_deferred("disabled", true)  # Disable the CollisionShape2D
-			in_coll = false
-		else:
+			play_open = false
+			play_close = true
+			left_open = true
+			print("Process")
+			print("   play_open: ", play_open, "   play_close: ", play_close)
+			print("   right_open: ", right_open, "   left_open: ", left_open)
+		elif curr_side == 1 && left_open == false:
 			animLeft.play("OpenLeft")
 			collDoor.set_deferred("disabled", true)  # Disable the CollisionShape2D
-			in_coll = false
-		#collDetect.set_deferred("disabled", true)
-#	elif in_coll == false:
-#		collDoor.set_deferred("disabled", false)  # Disable the CollisionShape2D
-#		collDetect.set_deferred("disabled", false)
-		
-#	if $CollDoor.disabled == false:
-#		print("entered collDoor is enabled")
-#	else:
-#		print("entered collDoor is disabled")
-#	if $PlayerDetection/CollisionShape2D.disabled == false:
-#		print("entered collDetect is enabled")
-#	else:
-#		print("entered collDetect is disabled")
+			play_open = false
+			play_close = true
+			right_open = true
+			print("Process")
+			print("   play_open: ", play_open, "   play_close: ", play_close)
+			print("   right_open: ", right_open, "   left_open: ", left_open)
 	
 
 func _on_PlayerDetectionFromLeft_body_entered(body):
-	print("Entered")
+	print("Entered from left")
 	if body.name == "Player":  # Check if the colliding body is the player
-		in_coll = true
+		if !play_close:
+			play_open = true
+		if right_open:
+			stack_approach_side.insert(0,0)
+		else:
+			stack_approach_side.append(0)
+	var result = ""
+	for element in stack_approach_side:
+		result += str(element) + " "
+	print("   stack elems:", result)
+	print("   play_open: ", play_open, "   play_close: ", play_close)
+	print("   right_open: ", right_open, "   left_open: ", left_open)
 		
 func _on_PlayerDetectionFromRight_body_entered(body):
-	pass # Replace with function body.
+	print("Entered from right")
+	if body.name == "Player":  # Check if the colliding body is the player
+		if !play_close:
+			play_open = true
+		if left_open:
+			stack_approach_side.insert(0,1)
+		else:
+			stack_approach_side.append(1)
+	var result = ""
+	for element in stack_approach_side:
+		result += str(element) + " "
+	print("   stack elems:", result)
+	print("   play_open: ", play_open, "   play_close: ", play_close)
+	print("   right_open: ", right_open, "   left_open: ", left_open)
+
 
 func _on_PlayerDetectionFromLeft_body_exited(body):
-	print("Exited")
-	if body.name == "Player" && in_coll == false:
-		if coll_from_left == true:
-			animRight.play("CloseRight")
-			collDoor.set_deferred("disabled", false)  # Disable the CollisionShape2D
-			in_coll = false
-			coll_from_left = false
-		else:
-			animLeft.play("CloseLeft")
-			collDoor.set_deferred("disabled", false)  # Disable the CollisionShape2D
-			in_coll = false
-			coll_from_left = true
+	print("Exited from left")
+	if body.name == "Player":
+		curr_side = stack_approach_side[stack_approach_side.size() - 1]
+		if curr_side == 0:
+			if play_close:
+				animRight.play("CloseRight")
+				collDoor.set_deferred("disabled", false)  # Disable the CollisionShape2D
+				play_close = false
+				left_open = false
+			stack_approach_side.pop_back() 
+		elif curr_side == 1:
+			stack_approach_side.pop_front()
+	var result = ""
+	for element in stack_approach_side:
+		result += str(element) + " "
+	print("   stack elems:", result)
+	print("   play_open: ", play_open, "   play_close: ", play_close)
+	print("   right_open: ", right_open, "   left_open: ", left_open)
 
 
 func _on_PlayerDetectionFromRight_body_exited(body):
-	pass # Replace with function body.
+	print("Exited from right")
+	if body.name == "Player":
+		curr_side = stack_approach_side[stack_approach_side.size() - 1]
+		if curr_side == 1:
+			if play_close:
+				animLeft.play("CloseLeft")
+				collDoor.set_deferred("disabled", false)
+				play_close = false
+				right_open = false
+			stack_approach_side.pop_back() 
+		elif curr_side == 0:
+			stack_approach_side.pop_front()
+	var result = ""
+	for element in stack_approach_side:
+		result += str(element) + " "
+	print("   stack elems:", result)
+	print("   play_open: ", play_open, "   play_close: ", play_close)
+	print("   right_open: ", right_open, "   left_open: ", left_open)
