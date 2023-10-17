ff3aa06315f11870843f31a2543d32398f041dae
Labdhi Mehta <lmehta@emory.edu>
2023-10-17 15:17:50 -0400

    committing before pulling

--- cs 370/AnimatedSprite2D.gd
+++ cs 370/AnimatedSprite2D.gd
@@ -2,6 +2,7 @@ extends AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
--- cs 370/Heart.gd
+++ cs 370/Heart.gd
@@ -1,35 +1,22 @@
 extends CharacterBody2D
 
-
-@onready var _animated_sprite = $AnimatedSprite2D
-
+@onready var _animated_sprite = $AnimationPlayer
 @onready var timmy = $AudioStreamPlayer2D
 
+var bpm = 60	#bpm of music
+var bps = bpm / 60.0	#convert to beats per second
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
-
-	# Handle Jump.
-	#if Input.is_action_just_pressed("ui_accept"): #and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
 func _process(_delta):
+	
 	$AudioStreamPlayer2D.get_time()
 	var t = int(timmy.time)
-	print("Time is: ", t)
-	if(t < 15):
+	if(t < 8):
 		if(t % 4 == 0):
-			_animated_sprite.play("beat")
-	elif(t < 20):
-		_animated_sprite.play("beat")
+			_animated_sprite.speed_scale = bps
+			_animated_sprite.play("sword")
 	else:
-		if(t % 2 == 0):
-			_animated_sprite.play("beat")
+		_animated_sprite.speed_scale = bps * 2
+		_animated_sprite.play("sword")
+		
 		
 	
