e2d0b88d265bd39bf0c68939d4a7f03f5e3121f2
Isha Dahiya <rishita.dahiya.12@gmail.com>
2023-10-23 20:51:39 -0400

    test

---
+++ cs 370/Level 1/Animated Idle.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+# Animate all idle animation objects in level 1 setting
+
+# Get the child AnimatedSprite2D node which is a child of the CharacterBody2D
+# attached to this script
+@onready var anim = get_node("AnimatedSprite2D")
+
+# Play the "Idle" animation when the scene loads 
+func _ready():
+	anim.play("Idle")
