cbd09ec5664330e8a718cc2e8f41cd350112d739
cs329quiz0 <astarr4@emory.edu>
2023-10-31 18:17:07 -0400

    Continued work on inventory

--- cs 370/Inventory/InventorySlot.gd
+++ cs 370/Inventory/InventorySlot.gd
@@ -7,6 +7,9 @@ var child : Panel
 
 @onready var inventory = get_node("/root/Inventory")
 
+@onready var textInfoDisplay = get_parent().get_node("Item description")
+signal itemAnimPlay(item_id)
+
 func _ready():
 	var inventory_contents = inventory.get_inventory()
 	# Get and iterate through all child nodes
@@ -14,10 +17,8 @@ func _ready():
 	var i = 0
 	for key in inventory_contents.keys():
 		var value = inventory_contents[key]
-		print("Key:", key, "Value:", value)
-		# You can access and work with each child node here
+
 		child = get_child(i)
-		print("Child node name:", child.get_name())
 		
 		item = ItemClass.instantiate()
 		itemCurrent.emit(key)
@@ -25,3 +26,13 @@ func _ready():
 		
 		i += 1
 		
+
+
+func _on_slot_1_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed() && event.button_index == MOUSE_BUTTON_LEFT:
+			# Left mouse button is clicked on the panel
+			# Perform your actions here
+			print("Slot one clicked!")
+			textInfoDisplay.text = "Key: "
+			itemAnimPlay.emit("Key")
--- cs 370/Inventory/Item in inventory.gd
+++ cs 370/Inventory/Item in inventory.gd
@@ -1,28 +1,20 @@
 extends Node2D
 
 @onready var item = $ItemTexture
+@onready var anim = $ItemAnimation
 @onready var inventory = get_node("/root/Inventory")
 
 @onready var grid = get_parent().get_parent()
 signal itemCurrent(item_id)
 
+signal itemAnimPlay(item_id)
+
 func _ready():
 	
-	# Code for testing inventory only
-	# Randomly spawn items into the backend inventory to check their display in the frontend inventory
-#	if randi() % 2 == 0:
-#		inventory.add_item("Key")
-#	else:
-#		inventory.add_item("Blue")
-	print("Grid:", grid)
-	grid.itemCurrent.connect(_on_item_current)
+	anim.hide()
 	
-#	if inventory.has_item("Key"):
-#		item.texture = load("res://assets/Inventory/Key_yellow.png")
-#	#elif inventory.has_item("Blue"):
-#	else:
-#		#item.texture = load("res://assets/Inventory/Key_blue.png")
-#		item.texture = null
+	grid.itemCurrent.connect(_on_item_current)
+	grid.itemAnimPlay.connect(_on_item_anim_play)
 
 func _on_item_current(item_id):
 	print("Item in inventory slot: ", item_id)
@@ -30,3 +22,8 @@ func _on_item_current(item_id):
 		item.texture = load("res://assets/Inventory/Key_yellow.png")
 	else:
 		item.texture = null
+
+
+func _on_item_anim_play(item_id):
+	anim.show()
+	anim.play(item_id)
