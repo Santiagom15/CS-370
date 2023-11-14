4799e2f1b13fe204e9855f1eb711b5f321e2bba2
Santiago Martinez <smart74@emory.edu>
2023-11-13 20:30:15 -0500

    I have an error with animation and Idk why

--- cs 370/AnotherBoss/groundAttack.gd
+++ cs 370/AnotherBoss/groundAttack.gd
@@ -2,10 +2,6 @@ extends State
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func transition():
+	if !ray_cast.is_colliding():
+		get_parent().change_state('Idle')
--- cs 370/Idle.gd
+++ cs 370/Idle.gd
@@ -1,7 +1,7 @@
 extends State
 class_name IdleForAnotherboss
 
-
+@onready var test =  $"../Animations"
 func transition():# amethod from the parent class that we can use to transition to new states
 	if ray_cast.is_colliding():
 		get_parent().change_state("groundAttack")
@@ -10,5 +10,5 @@ func transition():# amethod from the parent class that we can use to transition 
 
 
 func animation():
-	animator.play("Idle")# why can't it find this animation 
+	test.play("Idle")# why can't it find this animation 
 	
