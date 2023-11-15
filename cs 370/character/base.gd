642fed24a01a3d05ed8e18f6bbcfa0faa41b83f7
asajo12 <aisha.sajo@emory.edu>
2023-11-15 14:11:36 -0500

    Merge branch 'Santi-aisha' of https://github.com/Santiagom15/CS-370 into aisha-sajo
    # Conflicts:
    #	cs 370/Level 1/floor5.tscn
    #	cs 370/character/base.gd
    #	cs 370/character/base.tscn

--- cs 370/character/base.gd
+++ cs 370/character/base.gd
@@ -3,34 +3,50 @@ extends CharacterBody2D
 # what you can do is  create global or non global variable that stays true as long as the farme you want is activated and false when it isn't
 @export var speed = 300
 @onready var _animated_sprite = $AnimatedSprite2D
-@onready var heart = $Camera2D/Heart
-@onready var timer =$Camera2D/Heart/Timer
+@onready var heart = $Camera2D/press/Heart
+@onready var timer =$Camera2D/press/Timer
+@onready var press =$Camera2D/press
 var heartbeat = 0.5
 var heartFrame = 1.0
 var space = false
 var moving = false
-var isFrame: bool
+var state:bool
 
 
+func _ready():
+	heart.play("heartBeat",0)
+	heart.set_frame(0)
+
+		
+func isSpacePressed()->bool:
+	var Space= Input.is_action_pressed("space")
+	if Space :
+		return true
+	else:
+		return false
+	
+		
+func _on_heart_frame_changed():
+	print("Moving: ",moving," space: ",isSpacePressed()," stat: ",state)
+	#if getState() and isSpacePressed():
+		#print("State: ",getState(), " Space: ",isSpacePressed())
+	
+func test():
+	print("Moving: ",moving," space: ",isSpacePressed()," stat: ",state)
+	
+	
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")	
 	if input_direction:
 		moving = true
 	else: 
 		moving = false
-		
-	var Space= Input.is_action_pressed("space")
-	if Space :
-		space = true 
-	else:
-		space = false
 	velocity = input_direction * speed
-
+	
+  
 func _physics_process(_delta):
-	get_input()
+	get_input()     
 	move_and_slide()
-
-
 
 func _process(_delta):
 	if Input.is_action_pressed("ui_right"):
@@ -44,12 +60,10 @@ func _process(_delta):
 	else:
 		_animated_sprite.pause()
 
-
-
-
-		
-		
-	
-		
-
-	
+func _on_timer_timeout():
+	if heart.get_frame() == 0:
+		heart.set_frame(1)# 1 is the light 
+		state = false
+	elif heart.get_frame() == 1:
+		heart.set_frame(0)# is dark 
+		state = true
