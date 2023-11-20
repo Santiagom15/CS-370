545a9fee317e9f05bdbec3eb279ac4f01c62dcc5
cs329quiz0 <astarr4@emory.edu>
2023-11-19 22:07:44 -0500

    Fixed missing dependency in first_game.tscn; removed demo button from this menu scene. Minor changes to a few other files

--- cs 370/character/PlayerButtons.gd
+++ cs 370/character/PlayerButtons.gd
@@ -12,11 +12,17 @@ func _ready():
 	# Display/render the buttons above other elements in the environment
 	for i in range(get_child_count()):
 		child_node = get_child(i)
-		child_node.z_index = 2
+		child_node.z_index = 1
+
 
 func _on_home_button_down():
+	print("Home button pressed")
 	get_tree().change_scene_to_file("res://Level Navigation/level_select.tscn")
-
 
 func _on_inventory_button_down():
 	get_tree().change_scene_to_file("res://Inventory/Inventory.tscn")
+
+
+func _on_level_button_down():
+	print("Level button pressed")
+	get_tree().change_scene_to_file("res://Level Navigation/level_select.tscn")
