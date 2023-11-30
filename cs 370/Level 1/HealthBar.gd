a65c727899900d38fb509f59495b40b3249173a6
Santiago Martinez <smart74@emory.edu>
2023-11-29 22:20:29 -0500

    health bar for my first boss now on its set to allison

--- cs 370/Level 1/HealthBar.gd
+++ cs 370/Level 1/HealthBar.gd
@@ -3,6 +3,7 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(_delta):
-	pass
+	print(camara.get_screen_center_position)
+	position = camara.get_screen_center_position ( )- Vector2(400,200)
 	#print(camara.postion,"Relative position ",global_position)
 
--- cs 370/character/character_body_2d.gd
+++ cs 370/character/character_body_2d.gd
@@ -1,42 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 @onready var _animated_sprite = $AnimatedSprite2D
+func get_input():
+	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")	
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")#Check how this works becasue our physics might be different 
-
-
+	
+  
 func _physics_process(_delta):
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta
-
-	# Handle Jump.
-	#if Input.is_action_just_pressed("ui_accept"): #and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionX = Input.get_axis("ui_left", "ui_right")
-	var directionY = Input.get_axis("ui_up","ui_down")
-	if directionX:
-		velocity.x = directionX * SPEED
-	elif directionY:
-		velocity.y=directionY * SPEED
-	else:
-		#understand these fucntions
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y=  move_toward(velocity.y,0,SPEED)
-
+	get_input()     
 	move_and_slide()
 
+
 func _process(_delta):
-	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_down") or Input.is_action_pressed("ui_up"):
-		_animated_sprite.play("run")
+	if Input.is_action_pressed("ui_right"):
+		_animated_sprite.play("walkright_aisha")
+	elif Input.is_action_pressed("ui_left"):
+		_animated_sprite.play("walkleft_aisha")
+	elif Input.is_action_pressed("ui_up"):
+		_animated_sprite.play("walk_backAisha")
+	elif Input.is_action_pressed("ui_down"):  
+		_animated_sprite.play("walk_fowardAisha")
 	else:
-		_animated_sprite.stop()
-	
-	
+		_animated_sprite.pause()
+		
