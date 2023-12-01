46fde3d8f0d9be35952712b81b5269b23763b3d3
asajo12 <aisha.sajo@emory.edu>
2023-11-30 21:02:30 -0500

    Healthbar implemented, need to fix camera to be still + change the boss

--- cs 370/AnotherBoss/spawnAttack.gd
+++ cs 370/AnotherBoss/spawnAttack.gd
@@ -6,6 +6,8 @@ var direction:Vector2
 var SelfCurrentPosition:Vector2
 var SelfPlayerPosition:Vector2
 
+@onready var global_var = get_node("/root/Global")
+
 func _ready():
 	animated.play("groundAttack")
 
@@ -20,3 +22,11 @@ func spawnAttack(PlayerPosition,CurrentPosition):
 	
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+
+func _on_body_entered(body):
+	if body.name =="player":
+		global_var.hits =global_var.hits + 1
+		if global_var.hits>=6:
+			get_tree().change_scene_to_file("res://deathscene.tscn")
+	
